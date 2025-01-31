a12a051bb3cef3b6b9a1b72c83f082f9aa7adf41
SANDI <sandi@SANDIs-MacBook-Pro.local>
2025-02-01 00:13:47 +0700

    push to repo

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int tambah(int a, int b);
+int dikalikan(int a, int b);
+
+int main() {
+    std::cout << tambah(5,6) << std::endl;
+    std::cout << dikalikan(59,36) << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
---
+++ operation.cpp
@@ -0,0 +1,7 @@
+int tambah(int a, int b) {
+    return a + b;
+}
+
+int dikalikan(int a, int b) {
+    return a * b;
+}
