8ea1ec37903a751e890e1e2d8422d7ad7e5e13fc
SANDI <sandi@SANDIs-MacBook-Pro.local>
2025-02-03 14:14:06 +0700

    add c_primer

---
+++ c_primer/input_output.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int v1 = 0, v2 = 0;
+    cout << "Enter a Number :" << endl;
+    cin >> v1;
+    cout << "Enter second Number :" << endl;
+    cin >> v2;
+    cout << "Total of number 1 and number 2 : " << v1 << "+" << v2 << "=" << v1+v2 << endl; 
+    return 0;
+}
