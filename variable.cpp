f3ac00eb9d9f1ec318ec9fec1f06383e388032f8
SANDI <sandi@SANDIs-MacBook-Pro.local>
2025-02-03 09:50:12 +0700

    add variable

---
+++ variable.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main () {
+    long a;
+    std::cout << "Input a number" << std::endl;
+    std::cin >> a;
+    std::cout << "Size number is :";
+    std::cout << sizeof(a) << std::endl;
+    return 0;
+
+}
