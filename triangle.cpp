cb88051788f1a329b272d5b367ba8e21dbff69cb
SANDI <sandi@SANDIs-MacBook-Pro.local>
2025-02-05 08:45:44 +0700

    add pattern triangle

--- triangle.cpp
+++ triangle.cpp
@@ -4,11 +4,25 @@ using namespace std;
 
 int main () {
     int leng = 10;
+    
+    //Pola 1
     for (int i = 0; i <= leng; ++i) {
-        for( int j= 0; j <= i ;++j) {
-            cout << "*";
-        }
-        cout << endl;
+         for( int j= 0; j <= i ;++j) {
+             cout << "*";
+         }
+         cout << endl;
     }
+
+    //Pola 2
+    for (int i = 0; i <= leng; ++i) {\
+        int space = leng - i;
+         for( int j= 0; j <= space ;++j) {
+             cout << " ";
+         };
+         for( int h= 0; h <= i ;++h) {
+             cout << "*";
+         }  
+         cout << endl;
+     }
     return 0;
 }
